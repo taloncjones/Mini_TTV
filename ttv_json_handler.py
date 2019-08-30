307526124241a403690e22143d82c893da1342da
Talon120 <taloncjones@gmail.com>
2019-08-30 12:05:51 -0700

    Fixed use of response objects

--- ttv_json_handler.py
+++ ttv_json_handler.py
@@ -2,9 +2,9 @@
 def combine_json(state, streams=None, games=None, follows=None, *args):
     data = {}
     data.update({'login': state})
-    if streams: data.update({'streams': streams.json()})
-    if games: data.update({'games': games.json()})
-    if follows: data.update({'follows': follows.json()})
+    if streams: data.update({'streams': streams})
+    if games: data.update({'games': games})
+    if follows: data.update({'follows': follows})
     for index, arg in args:
-        data.update({index: arg.json()})
+        data.update({index: arg})
     return data
