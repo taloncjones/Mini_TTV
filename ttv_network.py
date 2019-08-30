92bc2ac1767c8a40070772e946c20e657dedd124
Talon120 <taloncjones@gmail.com>
2019-08-30 12:41:29 -0700

    Update comments

--- ttv_network.py
+++ ttv_network.py
@@ -1,8 +1,10 @@
-# Create Header based on key, value provided
+# Create header for client-id
 def create_client_header(client_id):
     header = {"Client-ID": client_id}
     return header
 
+
+# Create header for auth token
 def create_auth_header(token):
     header = {"Authorization": "OAuth " + token}
     return header
