5ead31569fe9d10f5817beddff7cdd7485ebf704
Talon120 <taloncjones@gmail.com>
2019-08-30 18:17:27 -0700

    Update functionality and use external function

--- ttv_credentials.py
+++ ttv_credentials.py
@@ -1,4 +1,6 @@
-import json
+import json, random, logging, string
+from flask import session as login_session
+from ttv_api_calls import ttv_get_auth_code
 
 
 # Load Twitch App Client-ID from ttv_client_secrets.json and return client ID
@@ -11,3 +13,16 @@ def load_client_id():
 def load_client_secret():
     app_secret = json.loads(open('ttv_client_secrets.json', 'r').read())['web']['app_secret']
     return app_secret
+
+
+def get_user_auth():
+    state = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in range(30))
+    login_session['state'] = state
+
+    logging.debug("Received Login Request. State: %s" % login_session['state'])
+    return ttv_get_auth_code(state, load_client_id())
+
+
+def get_auth_token():
+
+    return
