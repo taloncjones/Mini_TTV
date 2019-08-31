cfd3bf15a3de5010cd2293aa053d3f03415d75d4
Talon120 <taloncjones@gmail.com>
2019-08-30 18:18:01 -0700

    Add custom redirect function

--- ttv_network_handler.py
+++ ttv_network_handler.py
@@ -1,3 +1,6 @@
+from flask import redirect
+import logging
+
 # Create header for client-id
 def create_client_header(client_id):
     header = {"Client-ID": client_id}
@@ -8,3 +11,9 @@ def create_client_header(client_id):
 def create_auth_header(token):
     header = {"Authorization": "OAuth " + token}
     return header
+
+
+# Redirect to URL
+def url_redirect(url):
+    logging.debug("Redirected to: " + url)
+    return redirect(url)
