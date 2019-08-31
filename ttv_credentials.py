6d9cdb0cb0cb987b75cd4b6fadc59242aad6ec36
Talon120 <taloncjones@gmail.com>
2019-08-30 18:49:55 -0700

    Import function from external file

--- ttv_credentials.py
+++ ttv_credentials.py
@@ -1,6 +1,7 @@
 import json, random, logging, string
 from flask import session as login_session
-from ttv_api_calls import ttv_get_auth_code
+from ttv_api_calls import ttv_get_auth_code, ttv_get_auth_token
+from ttv_network_handler import get_params
 
 
 # Load Twitch App Client-ID from ttv_client_secrets.json and return client ID
@@ -24,5 +25,23 @@ def get_user_auth():
 
 
 def get_auth_token():
+    logging.debug("Received Auth")
+    params = get_params()
+    state = params.get('state')
 
+    if ('state' not in login_session) or ('state' in login_session and login_session['state'] != state):
+        # Insert error condition. User not logged in or state != session state (manual /auth call)
+        pass
+
+    code = params.get('code')
+    json_response = ttv_get_auth_token(load_client_id(), load_client_secret(), code)
+
+    access_token, expires_in, refresh_token = json_response.get('access_token'), \
+                                              json_response.get('expires_in'), \
+                                              json_response.get('refresh_token')
+    logging.debug("AT: %s\nEx: %s\nRT: %s" % (access_token, expires_in, refresh_token))
+
+    login_session['access_token'] = access_token
+    login_session['token_expiration'] = expires_in
+    login_session['refresh_token'] = refresh_token
     return
