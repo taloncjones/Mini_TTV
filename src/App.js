e0ab1e5707eb9cc635caf1b57723d999e6679829
Talon120 <taloncjones@gmail.com>
2019-11-30 19:00:25 -0800

    Update and display template files

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import axios from 'axios';
+import Header from './components/layout/Header';
+import About from './components/pages/About';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>App</h1>
-      </div>
+      <Router>
+        <div className="App">
+          <div className="container">
+            <Header />
+            <Route exact path="/" />
+            <Route path="/about" component={About} />
+          </div>
+        </div>
+      </Router>
     );
   }
 }
--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { linkSync } from 'fs';
+import { Link } from 'react-router-dom';
 
 function Header() {
   return (
