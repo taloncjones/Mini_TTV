dcfc61d88c3c453cea8bbf462c50fb65a7d9706e
Talon120 <taloncjones@gmail.com>
2019-12-01 17:06:52 -0800

    Add get_user_info() function and route

--- mini_ttv.py
+++ mini_ttv.py
@@ -4,7 +4,7 @@ from flask import Flask, redirect, url_for, render_template
 from flask import session as login_session
 from flask_cors import CORS
 
-from ttv_api_calls import ttv_validate_token, ttv_live_follows, ttv_top_games, ttv_top_streams
+from ttv_api_calls import ttv_validate_token, ttv_live_follows, ttv_top_games, ttv_top_streams, ttv_get_user_info
 from ttv_credentials import load_client_id, get_user_auth, get_auth_token, user_disconnect
 from ttv_json_handler import combine_json
 
@@ -64,6 +64,12 @@ def follows():
             logging.debug("Error: %s" % e)
             disconnect()
     return {}
+
+
+# User info
+@app.route('/user/<int:user_id>')
+def user(user_id):
+    return ttv_get_user_info(client_id=load_client_id(), user_id=user_id)
 
 
 # Combine all json responses
