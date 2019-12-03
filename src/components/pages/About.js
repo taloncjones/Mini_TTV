f8a2251053a14a7ba51e4d168f877d960a099278
Talon120 <taloncjones@gmail.com>
2019-12-02 22:06:00 -0800

    Update CSS for About.js

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -3,11 +3,13 @@ import { Link } from 'react-router-dom';
 
 export default function About() {
   return (
-    <React.Fragment>
-      <h1>About</h1>
-      <p>This is my implementation of a mini TwitchTV player. The purpose of this project was to develop a backend to handle Twitch API calls, credentials, and JSON REST endpoint for a front-end component. The front end was initially static HTML, but I am developing a React front-end to learn and practice.</p>
-      <p>Mini_TTV v1.0.0</p>
-      <Link to={"//www.github.com/taloncjones/Mini_TTV"} target="_blank">Github for Mini_TTV</Link>
-    </React.Fragment>
+    <div className="about-page">
+      <div className="about-container">
+        <h1>About</h1>
+        <p>This is my implementation of a mini TwitchTV player. The purpose of this project was to develop a backend to handle Twitch API calls, credentials, and JSON REST endpoint for a front-end component. The front end was initially static HTML, but I am developing a React front-end to learn and practice.</p>
+        <p>Mini_TTV v1.0.0</p>
+        <Link to={"//www.github.com/taloncjones/Mini_TTV"} target="_blank">Github for Mini_TTV</Link>
+      </div>
+    </div>
   )
 }
