108f84cf9a025f09bc37df483ca484a852fae484
Talon120 <taloncjones@gmail.com>
2019-12-02 21:53:49 -0800

    Move CSS from Header.js to App.css

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -3,23 +3,11 @@ import { Link } from 'react-router-dom';
 
 function Header() {
   return (
-    <header style={headerStyle}>
+    <header className="header">
       <h1>Mini TTV</h1>
-      <Link style={linkStyle} to="/">Home</Link> | <Link style={linkStyle} to="/about">About</Link> | <Link style={linkStyle} to={"//127.0.0.1/login"} target="_blank">Log In/Out</Link>
+      <Link to="/">Home</Link> | <Link to="/about">About</Link> | <Link to={"//127.0.0.1/login"} target="_blank">Log In/Out</Link>
     </header>
   )
-}
-
-const headerStyle = {
-  background: '#222',
-  color: '#fff',
-  textAlign: 'center',
-  padding: '10px'
-}
-
-const linkStyle = {
-  color: '#fff',
-  textDecoration: 'none'
 }
 
 export default Header
