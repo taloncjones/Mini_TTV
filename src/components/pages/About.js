09f5b2ad567867c3af31a97717bff1341f992fdf
Talon120 <taloncjones@gmail.com>
2019-12-02 22:07:53 -0800

    Update GitHub link text

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -8,7 +8,7 @@ export default function About() {
         <h1>About</h1>
         <p>This is my implementation of a mini TwitchTV player. The purpose of this project was to develop a backend to handle Twitch API calls, credentials, and JSON REST endpoint for a front-end component. The front end was initially static HTML, but I am developing a React front-end to learn and practice.</p>
         <p>Mini_TTV v1.0.0</p>
-        <Link to={"//www.github.com/taloncjones/Mini_TTV"} target="_blank">Github for Mini_TTV</Link>
+        <Link to={"//www.github.com/taloncjones/Mini_TTV"} target="_blank">Click Here to View GitHub for Mini_TTV</Link>
       </div>
     </div>
   )
