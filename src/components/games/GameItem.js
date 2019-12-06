81b9de41d2e22f61e870d0541c4d6393ae7a4dcd
Talon120 <taloncjones@gmail.com>
2019-12-05 21:44:53 -0800

    Get total viewer count and display on poster

--- src/components/games/GameItem.js
+++ src/components/games/GameItem.js
@@ -3,12 +3,22 @@ import axios from 'axios';
 
 export class GameItem extends Component {
   state = {
-    info: []
+    info: [],
+    total: 0
   }
 
   componentDidMount() {
     axios.get('//127.0.0.1/game_info/' + this.props.game.id)
-      .then(res => this.setState({ info: res.data['data'][0] }))
+      .then(res => {
+        this.setState({ info: res.data['data'] })
+        var i;
+        var t = 0;
+        console.log(this.state.info)
+        for(i = 0; i < this.state.info.length; i++) {
+          t += this.state.info[i]['viewer_count'];
+        }
+        this.setState({ total: this.getViewerCount(t) })
+      })
   }
 
   getThumbnail(url) {
@@ -27,6 +37,9 @@ export class GameItem extends Component {
       <div className="game-card">
         <a href={"//twitch.tv/directory/game/" + this.props.game.name} className="no-hover" >
           <img src={this.getThumbnail(this.props.game.box_art_url)} alt="" />
+          <div className="stream-overlay viewers">
+            {this.state.total}
+          </div>
         </a>
       </div>
     )
