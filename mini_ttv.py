b8cd689c3f6258e14af44e5f32a995f029765d8f
Talon120 <taloncjones@gmail.com>
2019-12-12 23:19:20 -0800

    Add additional routes and handlers

--- mini_ttv.py
+++ mini_ttv.py
@@ -1,10 +1,10 @@
 import logging
 
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, request
 from flask import session as login_session
 from flask_cors import CORS
 
-from ttv_api_calls import ttv_validate_token, ttv_live_follows, ttv_top_games, ttv_top_streams, ttv_get_user_info, ttv_get_game_info, ttv_get_game_viewer_info
+from ttv_api_calls import ttv_validate_token, ttv_live_follows, ttv_top_games, ttv_top_streams, ttv_get_user_info, ttv_get_game_info, ttv_get_game_viewer_info, ttv_get_my_info
 from ttv_credentials import load_client_id, get_user_auth, get_auth_token, user_disconnect
 from ttv_json_handler import combine_json
 
@@ -21,9 +21,17 @@ def state():
     return login_session['state'] if 'state' in login_session else ''
 
 
+# Add Access-Control-Allow-Credentials header
+@app.after_request
+def add_header(resp):
+    resp.headers['Access-Control-Allow-Credentials'] = 'true'
+    return resp
+
+
 # Log in handler with random state generator
 @app.route('/login')
 def login():
+    login_session['referer'] = request.headers.get("Referer")
     return get_user_auth()
 
 
@@ -31,14 +39,38 @@ def login():
 @app.route('/auth')
 def authenticate():
     get_auth_token()
-    return redirect(url_for('home_page'))
+    if 'referer' in login_session and login_session['referer']:
+        return redirect(login_session['referer'])
+    else:
+        return redirect(url_for('home_page'))
 
 
 # Log out handler
 @app.route('/disconnect')
 def disconnect():
+    referer = request.headers.get("Referer")
+    if 'referer' in login_session:
+        referer = login_session['referer']
     user_disconnect()
-    return redirect(url_for('home_page'))
+    if referer:
+        return redirect(referer)
+    else:
+        return redirect(url_for('home_page'))
+
+
+# Check cookie data
+@app.route('/whoami')
+def who_am_i():
+    if 'state' in login_session:
+        return ttv_get_my_info(token=login_session['access_token'], client_id=load_client_id())
+    else:
+        return '{"data": []}'
+
+
+# Check logged in
+@app.route('/loggedin')
+def logged_in():
+    return 'true' if 'state' in login_session else 'false'
 
 
 # Top streams
@@ -99,4 +131,5 @@ def home_page():
 if __name__ == '__main__':
     app.secret_key = 'super_secret_key'
     app.debug = True
+    app.session_cookie_name = "MiniTTV"
     app.run(host='0.0.0.0', port=80)
