0409d4baa205edbff2e5aeaea2755daaf2657af8
Talon120 <taloncjones@gmail.com>
2019-12-15 20:28:25 -0800

    Convert from class to function

--- src/components/layout/LayoutJoiner.js
+++ src/components/layout/LayoutJoiner.js
@@ -2,15 +2,11 @@ import React, { Component } from 'react';
 import Header from './Header';
 import MiniDrawer from './MiniDrawer';
 
-class LayoutJoiner extends Component {
-  render() {
-    return (
-      <div>
-        <Header loggedIn={this.props.loggedIn} />
-        <MiniDrawer />
-      </div >
-    )
-  }
+export default function LayoutJoiner(loggedIn) {
+  return (
+    <div>
+      <Header loggedIn={loggedIn} />
+      <MiniDrawer />
+    </div>
+  )
 }
-
-export default LayoutJoiner;
