92680520e1c8d1b7870094805903389204488e62
Talon120 <taloncjones@gmail.com>
2019-12-15 17:44:58 -0800

    Update header with material-ui template

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,13 +1,41 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+
+const useStyles = makeStyles(theme => ({
+  grow: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+}));
 
 function Header({ loggedIn }) {
+  const classes = useStyles();
+
   return (
-    <header className="header">
-      <h1>Mini TTV</h1>
-      <Link to="/">Home</Link> | <Link to="/about">About</Link> | <Link to="/login">{loggedIn ? 'Log Out' : 'Log In'}</Link><br />
-      <Link to="/monstercat">Monstercat</Link>
-    </header>
+    <div className={classes.grow}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.grow}>
+            Mini TwitchTV
+            </Typography>
+          <Link to="/">Home</Link> | <Link to="/about">About</Link> | <Link to="/login">{loggedIn ? 'Log Out' : 'Log In'}</Link><br />
+          <Link to="/monstercat">Monstercat</Link>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </div>
   )
 }
 
