d99f014bf69bef2535ff7c15c043305ae4f1fe6f
Talon120 <taloncjones@gmail.com>
2020-01-11 15:04:06 -0800

    Update grid sizing

--- src/components/games/GameList.js
+++ src/components/games/GameList.js
@@ -16,7 +16,7 @@ export default function GameList({ games }) {
     <div className={classes.grow}>
       <Grid container spacing={2}>
         {games.map((game) => (
-          <Grid item key={game.id} sm={2} xs={3}>
+          <Grid item key={game.id} md={2} sm={3}>
             <GameItem key={game.id} game={game} />
           </Grid>
         ))}
