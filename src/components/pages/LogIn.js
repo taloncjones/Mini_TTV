ccb084d7ed5eb4a1756c8f1af1bd709f35465e08
Talon120 <taloncjones@gmail.com>
2020-01-11 00:42:56 -0800

    Update login page CSS

--- src/components/pages/LogIn.js
+++ src/components/pages/LogIn.js
@@ -1,39 +1,88 @@
 import React, { Component } from 'react';
+import { withStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
+import Paper from '@material-ui/core/Paper';
+import Link from '@material-ui/core/Link';
+import Typography from '@material-ui/core/Typography';
 
 var mini_ttv_api = require('../Url.json')['url'];
+
+const styles = theme => ({
+  button: {
+    marginTop: theme.spacing(1),
+  },
+  paper: {
+    textAlign: 'center',
+    backgroundColor: theme.palette.secondary[900],
+    padding: theme.spacing(1),
+    color: theme.palette.text.primary,
+    position: 'relative',
+  },
+  profileImage: {
+    display: 'block',
+    borderRadius: '50%',
+  },
+  profileBox: {
+    overflow: 'hidden',
+    width: theme.spacing(10),
+    margin: '0 auto',
+  },
+  profileContainer: {
+    flex: '0 0 auto',
+  },
+  user: {
+    color: theme.palette.text.primary,
+    "&:hover": {
+      color: theme.palette.primary.main,
+    }
+  },
+});
 
 class LogIn extends Component {
   displayPage() {
     let link;
+    const { classes } = this.props;
+
     if (this.props.profileInfo.length === 0) {
       link = '//' + mini_ttv_api + '/login';
       return (
-        <div className="profile-card">
-          <h3>Logging in will allow you to view your followed channels.</h3>
-          <Button href={ link } variant="contained" color="primary">Log In to Twitch TV!</Button>
-        </div>
+        <Paper className={classes.paper}>
+          <Typography variant="body1">
+            Logging in will allow you to view your followed channels.
+          </Typography>
+          <Button href={link} variant="contained" color="primary" className={classes.button}>
+            Log In to Twitch TV!
+          </Button>
+        </Paper>
       )
     } else {
       link = '//' + mini_ttv_api + '/disconnect';
       return (
-        <div className="profile-card">
-          <h3>Hi</h3>
-          <div className="profile-name">{this.props.profileInfo.display_name}</div>
-          <div className="profile-container">
-            <a href={"/" + this.props.profileInfo.display_name}>
-              <div className="profile-box">
-                <img className="profile-image" src={this.props.profileInfo.profile_image_url} alt="" />
-              </div>
-            </a>
+        <Paper className={classes.paper}>
+          <Typography variant="h6">
+            Welcome
+          </Typography>
+          <Link variant="body1" href={"/" + this.props.profileInfo.display_name} className={classes.user}>
+            {this.props.profileInfo.display_name}
+          </Link>
+          <div className={classes.profileContainer}>
+            <div className={classes.profileBox}>
+              <Link href={"/" + this.props.profileInfo.display_name}>
+                <img className={classes.profileImage} src={this.props.profileInfo.profile_image_url} alt="" />
+              </Link>
+            </div>
           </div>
-          <Button href={ link } variant="contained" color="primary">Log Out?</Button>
-        </div>
+          <Button href={link} variant="contained" color="primary" className={classes.button}>
+            Log Out?
+          </Button>
+        </Paper>
       )
     }
   }
 
   render() {
+    const { classes } = this.props;
+
     return (
       <div className="about-page">
         <div className="about-container">
@@ -44,5 +93,5 @@ class LogIn extends Component {
   }
 }
 
-export default LogIn
+export default withStyles(styles)(LogIn)
 
