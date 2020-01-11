b0addd2520b5179c8cc67e183c73b9d563a3d9ec
Talon120 <taloncjones@gmail.com>
2020-01-11 01:55:15 -0800

    Update game list and games to use MUI and CSS in JS

--- src/components/games/GameItem.js
+++ src/components/games/GameItem.js
@@ -1,7 +1,31 @@
 import React, { Component } from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Link from '@material-ui/core/Link';
 import axios from 'axios';
 
 var mini_ttv_api = require('../Url.json')['url'];
+
+
+const styles = theme => ({
+  gameCard: {
+    color: theme.palette.text.primary,
+    position: 'relative',
+  },
+  overlay: {
+    position: 'absolute',
+    top: theme.spacing(.5),
+    padding: theme.spacing(0),
+    paddingLeft: theme.spacing(.5),
+    paddingRight: theme.spacing(.5),
+    borderRadius: theme.spacing(.5),
+    color: theme.palette.text.primary,
+  },
+  viewers: {
+    right: theme.spacing(1),
+    backgroundColor: 'grey',
+  },
+});
 
 export class GameItem extends Component {
   constructor(props) {
@@ -36,17 +60,18 @@ export class GameItem extends Component {
   }
 
   render() {
+    const { classes } = this.props;
     return (
-      <div className="game-card">
-        <a href={"//twitch.tv/directory/game/" + this.props.game.name} className="no-hover" >
+      <Grid item className={classes.gameCard} xs={12}>
+        <Link href={"//twitch.tv/directory/game/" + this.props.game.name}>
           <img src={this.getThumbnail(this.props.game.box_art_url)} alt="" />
-          <div className="stream-overlay viewers">
+          <div className={`${classes.overlay} ${classes.viewers}`}>
             {this.state.total}
           </div>
-        </a>
-      </div>
+        </Link>
+      </Grid>
     )
   }
 }
 
-export default GameItem
+export default withStyles(styles)(GameItem)
--- src/components/games/GameList.js
+++ src/components/games/GameList.js
@@ -1,16 +1,26 @@
-import React, { Component } from 'react'
+import React from 'react'
+import Grid from '@material-ui/core/Grid';
+import { makeStyles } from '@material-ui/core/styles';
 import GameItem from './GameItem'
 
-class GameList extends Component {
-  render() {
-    return (
-      <div className="game-list">
-        {this.props.games.map((game) => (
-          <GameItem key={game.id} game={game} />
-        ))}
-      </div>
-    )
+const useStyles = makeStyles(theme => ({
+  grow: {
+    flexGrow: 1,
   }
-}
+}))
 
-export default GameList
+export default function GameList({ games }) {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.grow}>
+      <Grid container spacing={2}>
+        {games.map((game) => (
+          <Grid item sm={3} xs={6}>
+            <GameItem key={game.id} game={game} />
+          </Grid>
+        ))}
+      </Grid>
+    </div>
+  )
+}
