aca23395823fc08c513dd71ce61ebe03203bae87
Talon120 <taloncjones@gmail.com>
2020-01-12 20:10:05 -0800

    Add cycle to tutorial windows

--- src/components/pages/Welcome.js
+++ src/components/pages/Welcome.js
@@ -2,6 +2,8 @@ import React from 'react'
 import Typography from '@material-ui/core/Typography'
 import Paper from '@material-ui/core/Paper'
 import Grid from '@material-ui/core/Grid'
+import Button from '@material-ui/core/Button'
+import Box from '@material-ui/core/Box'
 import { makeStyles } from '@material-ui/core/styles'
 
 const useStyles = makeStyles(theme => ({
@@ -35,6 +37,14 @@ const useStyles = makeStyles(theme => ({
 
 export default function Welcome() {
   const classes = useStyles();
+  const [tutorial, setTutorial] = React.useState(0);
+
+  const handleNextTutorialClick = (event, index) => {
+    setTutorial(index);
+    if (index === 4) {
+      window.location = '/streams';
+    }
+  };
 
   return (
     <div className={classes.page}>
@@ -44,18 +54,36 @@ export default function Welcome() {
             <Typography variant="h4">
               Welcome to Mini TwitchTV!
             </Typography>
+            <Box display={tutorial === 0 ? "block" : "none"}>
+              <Button onClick={event => handleNextTutorialClick(event, 1)}>Show me around!</Button>
+            </Box>
+            <Box display={tutorial === 1 ? "block" : "none"}>
+              <Button onClick={event => handleNextTutorialClick(event, 2)}>And?</Button>
+            </Box>
+            <Box display={tutorial === 2 ? "block" : "none"}>
+              <Button onClick={event => handleNextTutorialClick(event, 3)}>Neat!</Button>
+            </Box>
+            <Box display={tutorial === 3 ? "block" : "none"}>
+              <Button onClick={event => handleNextTutorialClick(event, 4)}>Cool! Let's watch stuff!</Button>
+            </Box>
           </Paper>
         </Grid>
       </Grid>
-      <Paper className={classes.drawer}>
-        Test
-      </Paper>
-      <Paper className={classes.login}>
-        Test
-      </Paper>
-      <Paper className={classes.toggle}>
-        Test
-      </Paper>
+      <Box display={tutorial === 1 ? "block" : "none"}>
+        <Paper className={classes.drawer}>
+          Test
+        </Paper>
+      </Box>
+      <Box display={tutorial === 2 ? "block" : "none"}>
+        <Paper className={classes.login}>
+          Test
+        </Paper>
+      </Box>
+      <Box display={tutorial === 3 ? "block" : "none"}>
+        <Paper className={classes.toggle}>
+          Test
+        </Paper>
+      </Box>
     </div>
   )
 }
